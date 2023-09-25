ffd019a3e86eb0a7bec5f08a049a8099193664a7
marcinfo <90268921+marcinfo@users.noreply.github.com>
2023-09-25 16:16:04 -0300

    conf roteamento

--- core/models.py
+++ core/models.py
@@ -26,8 +26,8 @@ class TbPragas(models.Model):
     id_praga = models.AutoField(primary_key=True)
     cultura = models.CharField(max_length=45, blank=True, null=True)
     especie = models.CharField(max_length=45, blank=True, null=True)
-    nome_comum = models.CharField(max_length=45)
-    nome_comum2 = models.CharField(max_length=45)
+    nome_comum = models.CharField(max_length=45, blank=True, null=True)
+    nome_comum2 = models.CharField(max_length=48, blank=True, null=True)
     class Meta:
         verbose_name = "Tabela de Praga"
         verbose_name_plural = "Tabela de Pragas"
@@ -39,7 +39,7 @@ class Tb_Registros(Base):
     id_ocorrencia = models.AutoField(primary_key=True)
     usuario =  models.CharField(max_length=45,editable=False)
     praga = models.CharField(max_length=40,choices=lista_praga,help_text='Selecione qual o tipo de praga esta contaminando.')
-    cultura =  models.CharField(verbose_name='Cultura',max_length=45,choices=lista_cultura,
+    cultura =  models.CharField(verbose_name='Cultura',max_length=48,choices=lista_cultura,
                                 help_text='Qual plantação foi contaminada?')
     status = models.CharField(verbose_name='A Praga Esta Controlada?', max_length=45, choices=CONTROLE_CHOICE,help_text='A praga esta controlada?')
     nome_propriedade = models.CharField(verbose_name='Nome da Propriedade:',max_length=60,
@@ -63,7 +63,7 @@ class Ocorrencias(Base):
     user = models.CharField(verbose_name='Usuário',max_length=45)
     data_registro =  models.DateField(verbose_name='Data da Ocorrência',help_text='Data em que foi visualizada a praga.')
     praga = models.CharField(max_length=40,choices=lista_praga,help_text='Selecione qual o tipo de praga esta contaminando.')
-    cultura =  models.CharField(name='Cultura',max_length=45,choices=lista_cultura,help_text='Qual plantação foi contaminada?')
+    cultura =  models.CharField(name='Cultura',max_length=48,choices=lista_cultura,help_text='Qual plantação foi contaminada?')
     status = models.CharField(verbose_name='Controlada?', max_length=45, choices=CONTROLE_CHOICE,help_text='A praga esta controlada?')
     nome_propriedade = models.CharField(verbose_name='Nome da Propriedade afetada',max_length=60,help_text='Nome da propriedade que esta sendo contaminada.')
     prejuizo=models.DecimalField(verbose_name='Total do prejuizo R$',max_digits=20, decimal_places=2,default=0.0,help_text='qual o valor do prejuizo?')
